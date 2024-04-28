5485136062bf439f4171fd49119b12dcb44c5d42
Raj Ojha <74981516+rajraviojha@users.noreply.github.com>
2024-04-28 19:03:00 +0530

    Update onemore.py

--- onemore.py
+++ onemore.py
@@ -6,7 +6,7 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")  # To run Chrome in headless mode (without GUI)
 
 # Path to the Chrome WebDriver executable (ensure it's in your PATH or specify the full path)
-chrome_driver_path = "/usr/local/bin/chromedriver"
+chrome_driver_path = "/usr/local/bin/webdriver"
 
 # Initialize Chrome WebDriver with options
 driver = webdriver.Chrome(executable_path=chrome_driver_path, options=chrome_options)
